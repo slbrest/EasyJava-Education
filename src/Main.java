78019990822aadf79c6f112b676516893bc6c97c
slbrest <sl48@mail.ru>
2022-12-04 21:31:15 +0100

    lesson 4

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner scr = new Scanner(System.in);
-//        System.out.println("Hi! Write a sentence to display on the screen.");
-//        String str = scr.nextLine();
-//        System.out.println(str);
-        System.out.println("Вы достигли уровня " + levelUp(5));
+
     }
 
-    public static int levelUp(int lvl) {
-        return lvl+1;
-    }
 }
