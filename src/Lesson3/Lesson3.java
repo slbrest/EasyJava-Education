f553a987d664832d36634582a6f279e982939f87
slbrest <sl48@mail.ru>
2022-12-04 20:25:13 +0100

    lesson 3

--- src/Lesson3/Lesson3.java
+++ src/Lesson3/Lesson3.java
@@ -1,12 +1,16 @@
 package Lesson3;
 
 public class Lesson3 {
-    public static int nextLevel = 1000;
-    public static int charExp = 100000;
+
     public static void main(String[] args) {
-        System.out.println(showLevel());
+        printLevel(showLevel(10000));
     }
-    public static int showLevel (){
-       return charExp / nextLevel;
+    public static int showLevel (int charExp){
+       return charExp / 1000;
     }
+
+    public static void printLevel (int level){
+        System.out.println("Уровень персонажа " + level);
+    }
+
 }
