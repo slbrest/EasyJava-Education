6c4257662cb0a7869347e4d45e7e3b3ca57259c6
slbrest <sl48@mail.ru>
2022-12-19 22:32:57 +0100

    lesson 5_1

--- src/Lesson6/Lesson6.java
+++ src/Lesson6/Lesson6.java
@@ -4,35 +4,22 @@ import java.util.Arrays;
 
 public class Lesson6 {
     public static void main(String[] args) {
-        int[] array = {1,90,15,60,21,11};
-        bubbleSort(array);
-        bubbleSortDesc(array);
+        int[] array = {8, 215, 12, 17, 13, 200};
+        sortBarrels(array);
     }
 
-    public static void bubbleSort(int[] array){
-        for (int i = array.length - 1; i >= 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if(array[j] > array[j+1]){
-                    int t = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = t;
-                }
+    public static void sortBarrels(int[] array){
+        int wineCount = 0;
+        for (int i : array){
+            if(i%2 == 0) wineCount++;
+        }
+        int[] wineArray = new int[wineCount];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if(array[i]%2 == 0){
+                wineArray[j] = array[i];
+                j++;
             }
         }
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(wineArray));
     }
-
-    public static void bubbleSortDesc(int[] array){
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = i+1; j < array.length; j++) {
-                if(array[i] < array[j]){
-                    int t = array[i];
-                    array[i] = array[j];
-                    array[j] = t;
-                }
-            }
-        }
-        System.out.println(Arrays.toString(array));
-    }
-
 }
