7b5966e04491a9333d64d435327a8699f521346a
slbrest <sl48@mail.ru>
2022-12-19 22:18:30 +0100

    lesson 5_1

--- src/Lesson6/Lesson6.java
+++ src/Lesson6/Lesson6.java
@@ -1,7 +1,38 @@
 package Lesson6;
 
+import java.util.Arrays;
+
 public class Lesson6 {
     public static void main(String[] args) {
-        System.out.println();
+        int[] array = {1,90,15,60,21,11};
+        bubbleSort(array);
+        bubbleSortDesc(array);
     }
+
+    public static void bubbleSort(int[] array){
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if(array[j] > array[j+1]){
+                    int t = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = t;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void bubbleSortDesc(int[] array){
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if(array[i] < array[j]){
+                    int t = array[i];
+                    array[i] = array[j];
+                    array[j] = t;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
 }
