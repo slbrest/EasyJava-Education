1eb58dcaa823388f91e79ed7d44381063e88982f
slbrest <sl48@mail.ru>
2022-12-22 22:09:58 +0100

    lesson 6

--- src/Lesson6/Lesson6.java
+++ src/Lesson6/Lesson6.java
@@ -6,8 +6,12 @@ public class Lesson6 {
     public static void main(String[] args) {
         int[] array = {8, 215, 12, 17, 13, 200};
         int[][] matrix = new int [5][5];
+        int[][] matrix2 = new int [7][7];
+        int[][] matrixSnail = new int [5][5];
         sortBarrels(array);
-        fillDias(matrix);
+        fillDiags(matrix);
+        fillDiags2(matrix2);
+        snailMatrix(matrixSnail);
     }
 
     public static void sortBarrels(int[] array){
@@ -25,10 +29,55 @@ public class Lesson6 {
         System.out.println(Arrays.toString(wineArray));
     }
 
-    public static void fillDias(int[][] array){
+    public static void fillDiags(int[][] array){
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 if(i == j) array[i][j] = 1;
+                if(i + j == array.length - 1) array[i][j] = 1;
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void fillDiags2(int[][] array){
+        for (int i = 0; i < array.length; i++) {
+            array[i][i] = 1;
+            array[i][array.length - 1 - i] = 1;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void snailMatrix(int[][] array){
+        int a = 0;
+        int n = array.length;
+        int m;
+        if(n%2 == 0) m = n / 2;
+        else m = (n + 1) / 2;
+        for (int i = 0; i < m; i++) {
+            for (int j = i; j < n - i; j++) { //на право
+                array[i][j] = a;
+                a++;
+            }
+            for (int j = i + 1; j < n - i; j++) { //вниз
+                array[j][n-i-1] = a;
+                a++;
+            }
+            for (int j = n - i - 2; j >= i; j--) { //налево
+                array[n-i-1][j] = a;
+                a++;
+            }
+            for (int j = n - i - 2; j > i ; j--) { //верх
+                array[j][i] = a;
+                a++;
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
