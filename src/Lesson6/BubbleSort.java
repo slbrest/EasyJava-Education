3219234c889315773ba398e3838ca4613f48edf9
slbrest <sl48@mail.ru>
2022-12-25 21:30:29 +0100

    lesson 6

--- src/Lesson6/BubbleSort.java
+++ src/Lesson6/BubbleSort.java
@@ -35,4 +35,13 @@ public class BubbleSort {
         System.out.println(Arrays.toString(array));
     }
 
+    public static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
