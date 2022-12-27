7012acea305317e3fc36ee4c2264b3ba538e08e2
slbrest <sl48@mail.ru>
2022-12-27 21:37:38 +0100

    lesson 6

--- src/Lesson7/Lesson7.java
+++ src/Lesson7/Lesson7.java
@@ -2,6 +2,13 @@ package Lesson7;
 
 public class Lesson7 {
     public static void main(String[] args) {
-      Player player = new Player("Zanoza", 1 , "Warrior");
+
+        Player player = new Player("Zanoza", 1 , "Warrior");
+
+        Player player1 = new Player("Zazazz", 1 , "Warrior");
+
+        System.out.println("Player " + player.name + " Player1 " + player1.name);
     }
+
+
 }
