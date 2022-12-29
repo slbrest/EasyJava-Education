de5023213144e85a3adad051729505003b0c7890
slbrest <sl48@mail.ru>
2022-12-29 20:59:31 +0100

    lesson 7

--- src/Lesson7/Lesson7.java
+++ src/Lesson7/Lesson7.java
@@ -5,7 +5,7 @@ public class Lesson7 {
 
         Player player = new Player("Zanoza", 1 , "Warrior");
 
-        Player player1 = new Player("Как все успеть в этой жизни???", 1 , "Warrior");
+        Player player1 = new Player("Новый год на носу", 1 , "Warrior");
 
         System.out.println("Player " + player.name + " Player1 " + player1.name);
     }
