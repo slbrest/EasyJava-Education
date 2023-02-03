5f1db9fa7fc79640c3f8c5da29bf203081a8c4b2
slbrest <sl48@mail.ru>
2023-02-03 08:13:43 +0100

    lesson 8

--- src/Lesson8/Lesson8.java
+++ src/Lesson8/Lesson8.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class Lesson8 {
     public static void main(String[] args) {
-        int[] passwords = {2021, 1023, 9929};
-        Scanner scanner = new Scanner(System.in);
-        int  password = scanner.nextInt();
-        for (int x = 0; true;) {
-            if (passwords[x] == password)
-            System.out.println("Welcome");
-        }
+        Scanner sc = new Scanner(System.in);
+        double c = sc.nextDouble();
+
+        System.out.println(fahr(c));
+    }
+    static double fahr (double cels){
+        double fahr = 1.8 * cels + 32;
+        return fahr;
     }
 }
