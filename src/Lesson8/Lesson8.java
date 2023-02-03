78c69745a686cff484e556d8e7b16164dffbcdc9
slbrest <sl48@mail.ru>
2023-02-03 10:54:55 +0100

    lesson 8

--- src/Lesson8/Lesson8.java
+++ src/Lesson8/Lesson8.java
@@ -3,14 +3,21 @@ package Lesson8;
 import java.util.Scanner;
 
 public class Lesson8 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double c = sc.nextDouble();
-
-        System.out.println(fahr(c));
+    static String isPrime(int number)
+    {
+        String result = "Is prime";
+            if((number / 2) >= 2){
+                if (number % number == 0) {
+                 result = "Isn't prime";
+        }
     }
-    static double fahr (double cels){
-        double fahr = 1.8 * cels + 32;
-        return fahr;
+        return result;//return the result
+    }
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int number = read.nextInt();
+        String output = isPrime(number);
+        System.out.println(output);
     }
 }
