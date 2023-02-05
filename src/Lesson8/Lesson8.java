7975e61de5c84fc0a2450be21d9c44f32d498529
slbrest <sl48@mail.ru>
2023-02-05 12:49:23 +0100

    lesson 8

--- src/Lesson8/Lesson8.java
+++ src/Lesson8/Lesson8.java
@@ -2,37 +2,26 @@ package Lesson8;
 
 import java.util.Scanner;
 
-/*Проинициализируй статические переменные следующими значениями:
+/*Выведи на экран следующий текст в две строки:
+It's a Windows path: "C:\Program Files\Java\jdk-13.0.0\bin"
+It's a Java string: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"
 
-partyFace — "\uD83E\uDD73"
-balloon — "\uD83C\uDF88"
-gift — "\uD83C\uDF81"
-partyPopper — "\uD83C\uDF89"
-cake — "\uD83C\uDF82"
-Чтобы посмотреть поздравление, запусти метод main().
+Подсказка:
+\” – экранирование двойной кавычки;
+\\ – экранирование обратной косой черты (\).
+
+Больше про экранирование символов и Escape-последовательности в Java читай в статье "Экранирование символов в Java".
 
 Требования:
-•	Нужно, чтобы переменная partyFace при объявлении была проинициализирована значением "\uD83E\uDD73".
-•	Нужно, чтобы переменная balloon при объявлении была проинициализирована значением "\uD83C\uDF88".
-•	Нужно, чтобы переменная gift при объявлении была проинициализирована значением "\uD83C\uDF81".
-•	Нужно, чтобы переменная partyPopper при объявлении была проинициализирована значением "\uD83C\uDF89".
-•	Нужно, чтобы переменная cake при объявлении была проинициализирована значением "\uD83C\uDF82".
+•	Нужно, чтобы программа выводила текст.
+•	Нужно, чтобы было выведено две строки.
+•	Текст первой строки должен быть: It's a Windows path: "C:\Program Files\Java\jdk-13.0.0\bin"
+•	Текст второй строки должен быть: It's a Java string: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"
 */
 
 public class Lesson8 {
-    public static String partyFace = "\uD83E\uDD73";
-    public static String balloon = "\uD83C\uDF88";
-    public static String gift = "\uD83C\uDF81";
-    public static String partyPopper = "\uD83C\uDF89";
-    public static String cake = "\uD83C\uDF82";
-
     public static void main(String[] args) {
-        printCongratulation();
-    }
-
-    public static void printCongratulation() {
-        String happyBirthday = "С днем рождения!";
-        String congratulation = partyFace + balloon + partyPopper + cake + gift;
-        System.out.println(happyBirthday + congratulation);
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"");
+        System.out.println("It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\"");
     }
 }
